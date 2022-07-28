7f2cab4cc09688aaa0fae74d41276d32020f15c3
OkoroPrecious <103753662+OkoroPrecious@users.noreply.github.com>
2022-07-28 13:03:29 +0100

    Java

---
+++ src/Seven_Segment/SegmentMain.java
@@ -0,0 +1,9 @@
+package Seven_Segment;
+
+public class SegmentMain {
+    public static void main(String[] args) {
+        SevenSegmentOutput segment = new SevenSegmentOutput();
+        segment.setLight("11111111");
+        segment.display();
+    }
+}
